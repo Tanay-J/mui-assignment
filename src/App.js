ee4e8aec548e372cd71863c6ca77246ce71c7b38
Tanay <tanay.joshi9@gmail.com>
2022-09-11 10:16:01 +0530

    feat: BillingDetails component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { Navbar } from "./components";
+import { Grid } from "@mui/material";
+import { BillingDetails, Navbar, OrderSummary } from "./components";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Grid
+        container
+        justifyContent="center"
+        alignContent="center"
+        sx={{ my: "80px" }}
+      >
+        <Grid item sx={{ px: "80px" }}>
+          <BillingDetails />
+        </Grid>
+        <Grid item sx={{ px: "80px" }} alignSelf="center">
+          <OrderSummary />
+        </Grid>
+      </Grid>
     </div>
   );
 }
