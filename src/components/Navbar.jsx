adffc6b418701276dec6854cadbf964f95b8e29a
Tanay <tanay.joshi9@gmail.com>
2022-09-11 11:19:28 +0530

    style: add icon to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import Brightness1Icon from "@mui/icons-material/Brightness1";
 
 function Navbar() {
   return (
     <>
       <AppBar position="relative" sx={{ backgroundColor: "white", py: 1 }}>
         <Toolbar>
-          <Container>
+          <Container sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <Brightness1Icon sx={{ color: "black" }}></Brightness1Icon>
             <Typography variant="h5" fontWeight="bold" sx={{ color: "black" }}>
               LOGO
             </Typography>
